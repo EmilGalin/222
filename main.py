59221449c8df49b6bc4648fb9f965e33d7530fe9
EmilGalin <92096829+EmilGalin@users.noreply.github.com>
2021-12-30 12:20:32 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,40 @@
-def main():
-    a, b, c = float(input('Введите a: ')), float(input('Введите b: ')), float(input('Введите c: '))
-    d = b**2 - 4*a*c
-    print('Дискриминант =', d)
-    if d < 0:
-        print('Корней нет')
-    elif d == 0:
-        x = -b / (2 * a)
-        print('x =', x)
+def maxS(arr):
+    S = 0
+    print(arr)
+    arr.sort(reverse=True)
+    print(arr)
+    for x in arr[::-1]:  # проверка на отриц. число начиная с меньшего
+        if x <= 0:
+            return "Невозможные значения в массиве: " + str(x) + "\n"
+    for i in range(0, len(arr) - 2):  # поиск наибольшего
+        if arr[i] < (arr[i + 1] + arr[i + 2]):
+            p = (arr[i] + arr[i + 1] + arr[i + 2]) / 2.0
+            S = (p * (p - arr[i]) * (p - arr[i + 1]) * (p - arr[i + 2])) ** 0.5
+            bordes = map(str, [arr[i], arr[i + 1], arr[i + 2]])
+            break
+    if (S == 0):
+        return "Треугольник создать невозможно\n"
     else:
-        x1 = (-b + d ** 0.5) / (2 * a)
-        x2 = (-b - d ** 0.5) / (2 * a)
-        print('x1 =', x1)
-        print('x2 =', x2)
+        return "Максимальная площадь: " + str(round(S, 6)) + "\nстороны: " + ", ".join(bordes) + "\n"
 
-if __name__ == "__main__":
+
+def main():
+    arr = [6, 1, 6, 5, 8, 4]
+    a = maxS(arr)
+    print(a)
+    arr = [2, 20, 7, 55,
+           1, 33, 12, -4]
+    a = maxS(arr)
+    print(a)
+    arr = [2, 20, 7, 55,
+           1, 33, 12, 4]
+    a = maxS(arr)
+    print(a)
+    arr = [33, 6, 20, 1, 8,
+           12, 5, 55, 4, 9]
+    a = maxS(arr)
+    print(a)
+
+
+if __name__ == '__main__':
     main()
-    
